fa6fb8a3473d8b709b966a5972493613ddd56f8a
Elahe Kooshafar <kooshafar.elahe@gmail.com>
2023-12-01 18:02:26 -0500

    Update papers.js

--- src/data/resume/papers.js
+++ src/data/resume/papers.js
@@ -5,7 +5,7 @@ const papers = [
     others: 'Reihaneh Rabbany, and Farimah Poursafaei',
     title: 'Temporal Graph Analysis with TGX',
     link: 'https://www.cs.mcgill.ca/',
-    where: 'WSDM Demo 202',
+    where: 'WSDM Demo 2024',
   },
 ];
 
