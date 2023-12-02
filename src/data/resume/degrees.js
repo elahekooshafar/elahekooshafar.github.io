a87eead8a91033185d95042d42c714fe73be7d7f
Elahe Kooshafar <kooshafar.elahe@gmail.com>
2023-12-01 22:54:19 -0500

    Update

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,13 +1,13 @@
 const degrees = [
   {
     school: 'McGill University',
-    degree: 'Research M.Sc. Computer Science',
+    degree: 'Research M.Sc., Computer Science',
     link: 'https://www.cs.mcgill.ca/',
     year: 'Ongoing',
   },
   {
     school: 'Sharif University of Technology',
-    degree: 'B.Sc. Computer Science',
+    degree: 'B.Sc., Computer Science',
     link: 'https://en.sharif.edu/',
     year: 2022,
   },
