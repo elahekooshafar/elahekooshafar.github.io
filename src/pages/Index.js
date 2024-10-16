172968a73b92724b94ead74a3777544f962a27f8
Elahe Kooshafar <kooshafar.elahe@gmail.com>
2024-10-16 13:32:03 -0400

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Elahe Kooshafar's personal website."}
+    description={"Elahe Kooshafar's personal website. "}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
           <h2><Link to="/">Bio</Link></h2>
-          <p>A highly motivated graduate student in Computer Science.</p>
+          <p> A highly motivated graduate student with a strong passion for research in
+            graph learning and deep learning.
+          </p>
         </div>
       </header>
-      <p>
-        Hi, I&apos;m Elahe, a final-year research master&apos;s student in Computer Science at 
-        <a href="https://mila.quebec/en/"> Mila - Quebec Artificial Intelligence Institute</a> and 
-        <a href="https://cs.mcgill.ca/"> McGill University</a>. I completed my bachelor&apos;s in Computer Science from 
-        <a href="https://www.sharif.ir/"> Sharif University of Technology</a> in 2022. 
-
-        My research interests include graph representation learning, graph neural networks, network science, and deep learning. 
-        Currently, I am focusing on temporal graph learning, developing machine learning models for complex and evolving networks.
-        {/* Feel free to <Link to="/contact">contact me</Link> for collaboration or inquiries. */}
+      <p> Hi, I&apos;m Elahe.
+        I&apos;m a research MSc student in Computer Science at <a href="https://cs.mcgill.ca/">McGill University</a> &nbsp;and
+        &nbsp;<a href="https://mila.quebec/en/">Mila - Quebec Artificial Intelligence Institute</a>.
+        Previously, I got my bachelors in Computer Science from Sharif University of Technology
+        in 2022.
+        I have a broad interest in graph representation learning, graph neural networks,
+        network science, and deep learning. My current research focus is on temporal
+        graph learning, which involves machine learning models for complex and
+        evolving networks in the real world.
+        {/* Please feel free to <Link to="/contact">contact me</Link> for collaboration or else. */}
       </p>
     </article>
   </Main>
