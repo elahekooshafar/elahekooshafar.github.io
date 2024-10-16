2483d3d61acc35a7089315722d734b52282cd949
Elahe Kooshafar <kooshafar.elahe@gmail.com>
2024-10-16 14:56:42 -0400

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -14,16 +14,16 @@ const Index = () => (
         </div>
       </header>
       <p>
-          I&apos;m a last year Research MSc student in Computer Science at
-          <a href="https://cs.mcgill.ca/">McGill University</a> &nbsp;and
-          &nbsp;<a href="https://mila.quebec/en/">Mila - Quebec Artificial Intelligence Institute</a>, supervised
-          by <a href="http://www.reirab.com/">Reihaneh Rabbany</a>. Previously, I got my bachelor&apos;s in Computer Science
-          from <a href="https://en.sharif.ir/">Sharif University of Technology</a>.
+        I&apos;m a last year Research MSc student in Computer Science at
+        <a href="https://cs.mcgill.ca/">McGill University</a> &nbsp;and
+        &nbsp;<a href="https://mila.quebec/en/">Mila - Quebec Artificial Intelligence Institute</a>, supervised
+        by <a href="http://www.reirab.com/">Reihaneh Rabbany</a>. Previously, I got my bachelor&apos;s in Computer Science
+        from <a href="https://en.sharif.ir/">Sharif University of Technology</a>.
 
-          I have a broad interest in graph representation learning, graph neural networks, network science,
-          and deep learning. I&apos;m currently working on temporal graph representation learning methods.
+        I have a broad interest in graph representation learning, graph neural networks
+        , network science, and deep learning. I&apos;m currently working on temporal
+        graph representation learning methods.
       </p>
-
 
     </article>
   </Main>
