3c755775222252cd161e5e3257f1f213b8472ce5
Elahe Kooshafar <kooshafar.elahe@gmail.com>
2024-10-16 02:40:15 -0400

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -13,7 +13,14 @@ const Index = () => (
           <h2><Link to="/">Bio</Link></h2>
         </div>
       </header>
-      <p> Hi, I&apos;m Elahe.
+      <p> I'm a last year research master's student in Computer Science at 
+      <a href="https://mila.quebec/en">Mila</a> and <a href="https://www.mcgill.ca/">McGill University</a> under 
+      the supervision of <a href="http://www.reirab.com/">Reihaneh Rabbany</a>. I got my bachelor's in Computer Science
+      from [Sharif University of Technology]<a href="https://www.sharif.ir/">Sharif University of Technology</a>. I have a
+        broad interest in graph representation learning, graph neural networks, network science, and deep learning. I'm currently
+          working on temporal graph representation learning methods.
+      
+      {/*Hi, I&apos;m Elahe.
         I&apos;m a research MSc student in Computer Science at <a href="https://cs.mcgill.ca/">McGill University</a> &nbsp;and
         &nbsp;<a href="https://mila.quebec/en/">Mila - Quebec Artificial Intelligence Institute</a>.
         Previously, I got my bachelors in Computer Science from Sharif University of Technology
@@ -21,7 +28,7 @@ const Index = () => (
         I have a broad interest in graph representation learning, graph neural networks,
         network science, and deep learning. My current research focus is on temporal
         graph learning, which involves machine learning models for complex and
-        evolving networks in the real world.
+        evolving networks in the real world.*/}
         {/* Please feel free to <Link to="/contact">contact me</Link> for collaboration or else. */}
       </p>
     </article>
