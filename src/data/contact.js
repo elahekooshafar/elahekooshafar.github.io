2e257992b328d14fc74bbe0e1ce5efbef40ca0f8
Elahe Kooshafar <kooshafar.elahe@gmail.com>
2024-10-16 17:06:42 -0400

    Update contact.js

--- src/data/contact.js
+++ src/data/contact.js
@@ -5,7 +5,7 @@ import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 // import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
 // import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
-import { faScholar } from '@fortawesome/free-regular-svg-icons/fa-google-scholar';
+import { faGraduationCap } from '@fortawesome/free-regular-svg-icons/faGraduationCap';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
 
@@ -23,7 +23,7 @@ const data = [
   {
     link: 'https://scholar.google.ca/citations?user=vuUUKUMAAAAJ&hl=en&oi=sra',
     label: 'Google Scholar',
-    icon: faScholar,
+    icon: faGraduationCap,
   },
   {
     link: 'mailto:elahe.kooshafar@mila.quebec',
