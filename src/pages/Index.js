4a500512af3cf5f39f08fa4844d1ba1e1243af7f
Elahe Kooshafar <kooshafar.elahe@gmail.com>
2024-10-16 03:35:24 -0400

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,26 +5,28 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Elahe Kooshafar's personal website. New York based Stanford ICME graduate, "}
+    description={"Elahe Kooshafar's personal website. "}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2><Link to="/">About me</Link></h2>
+          <h2><Link to="/">Bio</Link></h2>
           <p> A highly motivated graduate student with a strong passion for research in
             graph learning and deep learning.
           </p>
         </div>
       </header>
-      <p>
-        I&apos;m a last year research master&apos;s student in Computer Science
-        at <a href="https://mila.quebec/en/">Mila - Quebec Artificial Intelligence Institute</a> and 
-        <a href="https://cs.mcgill.ca/"> McGill University</a>. 
-        I have a broad interest in graph representation learning, graph neural networks, network science, and deep learning. 
-        I&apos;m currently working on temporal graph representation learning methods.
+      <p> Hi, I&apos;m Elahe.
+        I&apos;m a research MSc student in Computer Science at <a href="https://cs.mcgill.ca/">McGill University</a> &nbsp;and
+        &nbsp;<a href="https://mila.quebec/en/">Mila - Quebec Artificial Intelligence Institute</a>.
+        Previously, I got my bachelors in Computer Science from Sharif University of Technology
+        in 2022.
+        I have a broad interest in graph representation learning, graph neural networks,
+        network science, and deep learning. My current research focus is on temporal
+        graph learning, which involves machine learning models for complex and
+        evolving networks in the real world.
         {/* Please feel free to <Link to="/contact">contact me</Link> for collaboration or else. */}
       </p>
-
     </article>
   </Main>
 );
