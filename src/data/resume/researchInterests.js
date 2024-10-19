8f17ba8510cfe7cb152c2d27ee36554e996ebb13
Elahe Kooshafar <kooshafar.elahe@gmail.com>
2024-10-19 00:52:52 -0400

    Create researchInterests.js

---
+++ src/data/resume/researchInterests.js
@@ -0,0 +1,8 @@
+const researchInterests = [
+  'Temporal Graph Analysis',
+  'Machine Learning on Graphs',
+  'Spatio-Temporal Data Mining',
+  'Graph-based Recommender Systems',
+];
+
+export default researchInterests;
