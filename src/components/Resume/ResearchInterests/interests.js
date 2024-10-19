eb38053523e5724490f7000509c9b7e776f6e646
Elahe Kooshafar <kooshafar.elahe@gmail.com>
2024-10-19 01:10:35 -0400

    Update interests.js

--- src/components/Resume/ResearchInterests/interests.js
+++ src/components/Resume/ResearchInterests/interests.js
@@ -1,1 +1,20 @@
-ResearchInterests
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Interest = ({ data }) => (
+  <article className="interest-container">
+    <header>
+      <h4 className="interest">{data.area}</h4>
+      <p className="description">{data.description}</p>
+    </header>
+  </article>
+);
+
+Interest.propTypes = {
+  data: PropTypes.shape({
+    area: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Interest;
