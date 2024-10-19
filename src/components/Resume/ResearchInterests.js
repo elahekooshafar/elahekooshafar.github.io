784106e29eaf7b6c56921cd4bf3e9fc204bd921c
Elahe Kooshafar <kooshafar.elahe@gmail.com>
2024-10-19 01:03:12 -0400

    Create ResearchInterests.js

---
+++ src/components/Resume/ResearchInterests.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ResearchInterests = ({ data }) => (
+  <div className="research-interests">
+    <div className="link-to" id="research-interests" />
+    <div className="title">
+      <h3>Research Interests</h3>
+    </div>
+    <ul>
+      {data.map((interest, index) => (
+        <li key={index}>{interest}</li>
+      ))}
+    </ul>
+  </div>
+);
+
+ResearchInterests.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.string),
+};
+
+ResearchInterests.defaultProps = {
+  data: [],
+};
+
+export default ResearchInterests;
